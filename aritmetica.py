86a75b70edd1aff69ffd213a40f6a8283552a5c4
Roberto Duran Añasco <86730335+Rbtski@users.noreply.github.com>
2021-08-17 19:59:43 -0400

    Add files via upload

---
+++ aritmetica.py
@@ -0,0 +1,42 @@
+print ("suma: ")
+numero_uno = 5
+numero_dos = 10
+resultado = numero_uno + numero_dos
+print("El resultado de la suma es: " + str(resultado))
+
+
+print ("resta: ")
+numero_uno = 5
+numero_dos = 10
+resultado = numero_uno - numero_dos
+print("El resultado de la resta es: " + str(resultado))
+
+print ("Multiplicar: ")
+numero_uno = 5
+numero_dos = 10
+resultado = numero_uno * numero_dos
+print("El resultado de la Multiplicacion es: " + str(resultado))
+
+print ("dividir: ")
+numero_uno = 5
+numero_dos = 2
+resultado = numero_uno / numero_dos
+print("El resultado de la División es: " + str(resultado))
+
+print ("division Entera: ")
+numero_uno = 6
+numero_dos = 2
+resultado = numero_uno // numero_dos
+print("El resultado de la División Entera es: " + str(resultado))
+
+print ("Exponente: ")
+numero_uno = 5
+numero_dos = 2
+resultado = numero_uno ** numero_dos
+print("El resultado de la Exponencial es: " + str(resultado))
+
+print ("Modulo: ")
+numero_uno = 30
+numero_dos = 8
+resultado = numero_uno % numero_dos
+print("El resultado del Modulo es: " + str(resultado))
